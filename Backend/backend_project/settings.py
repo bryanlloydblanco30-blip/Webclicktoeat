ca598e587a267688060058badbd9524cdfd1845d
Bryan Lloyd <bryanlloydblanco30@gmail.com>
2025-12-03 19:44:10 +0800

    Add Webclicktoeat files

--- Backend/backend_project/settings.py
+++ Backend/backend_project/settings.py
@@ -29,7 +29,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -98,6 +98,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://clicktoeat-pw67.onrender.com",
     "https://clicktoeat-frontend.onrender.com",
     "https://clicktoeat-admin.onrender.com",
+    "https://webclicktoeat-6htejj2if-bryans-projects-e4c7e470.vercel.app"
     
     # Vercel deployments - All current frontend/admin URLs
     "https://clickto-eat.vercel.app",  # Main frontend
@@ -140,6 +141,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://clicktoeat-pw67.onrender.com",
     "https://clicktoeat-frontend.onrender.com",
     "https://clicktoeat-admin.onrender.com",
+    "https://webclicktoeat-6htejj2if-bryans-projects-e4c7e470.vercel.app"
+    "https://webclicktoeat.vercel.app"
     
     # Vercel deployments
     "https://clickto-eat.vercel.app",
