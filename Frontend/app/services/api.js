06b3eec09a606d69dda0b77007c621a474f53fc4
Bryan Lloyd <bryanlloydblanco30@gmail.com>
2025-12-03 20:22:24 +0800

    another

--- Frontend/app/services/api.js
+++ Frontend/app/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://clicktoeat-pw67.onrender.com';
+const API_BASE_URL ='https://clicktoeat-pw67.onrender.com';
 
 
 // ==================== HELPER FUNCTIONS ====================
